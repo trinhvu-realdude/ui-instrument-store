3e1a68945954d3b7bf7c3aa6cc5c7cc0269cd4ca
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2022-05-08 09:12:10 +0700

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HeaderComponent } from './partial/header/header.component';
+import { FooterComponent } from './partial/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
